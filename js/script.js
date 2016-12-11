ace72e62fff89bce9f1837aa0c22421a0a371405
Natalya Ivanova <natalyas.ivanova@gmail.com>
2016-12-11 17:27:16 +0200

    Move custom js code into script.js

--- js/script.js
+++ js/script.js
@@ -48,13 +48,43 @@ $(function() {
     });
 
     // JS custom checkbox
-    $(".js-checkbox").change(function() {
-        console.log("active checkbox");
+    $(".js-checkbox").mousedown(function() {
+    /* при клике на чекбоксе меняем его вид и значение */
+        changeCheck($(this));
     });
 
-    $("#js-checkbox-input").change(function() {
-        console.log($(this).is(":checked"));
+    $(".js-checkbox").each(function() {
+    /* при загрузке страницы нужно проверить какое значение имеет чекбокс и в соответствии с ним выставить вид */
+         changeCheckStart($(this));
     });
+
+    // Helpers for js checkbox
+    function changeCheck(el) {
+        var container = el.find(".js-checkbox-container"),
+        input = el.find(".js-native-checkbox"),
+        message = el.find(".message");
+
+        if (!input.attr("checked")) {
+            console.log("not checked");
+            container.html('\u2228');
+            input.attr("checked", true);
+            message.css("display", "block");
+        }
+        else {
+            container.html("");
+            input.attr("checked", false);
+            message.css("display", "none");
+        }
+    }
+
+    function changeCheckStart(el) {
+        var container = el.find(".js-checkbox-container"),
+        input = el.find(".js-native-checkbox");
+
+        if (input.attr("checked")) {
+            container.html("\u2228");
+        }
+    }
 
 
 }); // end of ready
